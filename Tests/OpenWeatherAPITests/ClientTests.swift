9347ac7dcce7172941ed60a6e8cfe55fe5914e57
Matt Boran <mattboran@gmail.com>
2023-02-27 19:31:38 -0700

    Add test for url formatting with other parameters

--- Tests/OpenWeatherAPITests/ClientTests.swift
+++ Tests/OpenWeatherAPITests/ClientTests.swift
@@ -9,9 +9,22 @@ final class ClientTests: XCTestCase {
 
     let dataService = DataServiceMock()
 
-    func testBasicURLFormatting() throws {
-        let client = Client(apiKey: Self.apiKey, dataService: dataService)
+    func testURLFormatting() throws {
+        var client = Client(apiKey: Self.apiKey, dataService: dataService)
         client.fetchWeather(latitude: Self.latitude, longitude: Self.longitude, completion: nil)
-        XCTAssertEqual(dataService.queryURL?.absoluteString, "https://api.openweathermap.org/data/3.0/onecall?lat=39.7392&lon=-104.9903&appid=TEST_API_KEY&units=standard")
+        XCTAssertEqual(
+            dataService.queryURL?.absoluteString,
+            "https://api.openweathermap.org/data/3.0/onecall?lat=39.7392&lon=-104.9903&appid=TEST_API_KEY&units=standard")
+
+        client = Client(
+            apiKey: Self.apiKey,
+            exclude: [.alerts, .daily, .hourly, .minutely],
+            units: .metric,
+            language: .romanian,
+            dataService: dataService)
+        client.fetchWeather(latitude: Self.latitude, longitude: Self.longitude, completion: nil)
+        XCTAssertEqual(
+            dataService.queryURL?.absoluteString,
+            "https://api.openweathermap.org/data/3.0/onecall?lat=39.7392&lon=-104.9903&appid=TEST_API_KEY&exclude=alerts,daily,hourly,minutely&units=metric&lang=ro")
     }
 }
